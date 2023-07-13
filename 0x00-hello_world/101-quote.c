851a5c048c5ab6546a2a13ecd70fcfb19e85180e
bookie212 <nancybookie675@gmail.com>
2023-07-13 07:33:02 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
-	return (0);
+	printf("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
