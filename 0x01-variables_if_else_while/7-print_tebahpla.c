21dfdf1e9406bd40cc671401aa6554ac10579be4
bookie212 <nancybookie675@gmail.com>
2023-07-14 10:59:25 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	for (n = 122; n <= 97; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
