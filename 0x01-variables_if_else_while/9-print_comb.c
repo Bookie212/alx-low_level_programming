4bfbe620c8451ac61c6cc2702f2fe3fc2b439b4c
bookie212 <nancybookie675@gmail.com>
2023-07-14 11:46:04 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
