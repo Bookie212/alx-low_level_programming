ae504749487dd596304eebaf4f32860527ae40cf
bookie212 <nancybookie675@gmail.com>
2023-07-14 14:46:32 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = '0'; n < '9'; n++)
+	for (n = 0; n < 9; n++)
 	{
-		for (m = n + 1; n <= '9'; m++)
+		for (m = n + 1; n < 10; m++)
 		{
 			if (m != n)
 			{
-				putchar(n);
-				putchar(m);
-				if (n == '8' && m == '9')
-				continue;
+				putchar((n % 10) + '0');
+				putchar((m % 10) + '0');
+				if (n == 8 && m == 9)
+					continue;
 				{
-					putchar(',');
-					putchar(' ');
+				putchar(',');
+				putchar(' ');
 				}
 			}
 		}
