8d09e79ea74de25a01e5269a7cf09552b871a885
bookie212 <nancybookie675@gmail.com>
2023-07-14 17:42:41 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,9 +17,9 @@ int main(void)
 			putchar(n + '0');
 			putchar(m + '0');
 			if (n < 8 || m < 9)
-				putchar(',');
-				putchar(' ');
-		}
+			putchar(',');
+			putchar(' ');
+		}               	
 	}
 	putchar('\n');
 	return (0);
