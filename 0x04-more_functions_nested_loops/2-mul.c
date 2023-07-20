9e5bdedbf8b2660a77eaceeabe775b2b2fbde7f5
bookie212 <nancybookie675@gmail.com>
2023-07-20 12:34:00 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiples two numbers
+ * @a: integer number
+ * @b: integer number
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	_putchar("%d\n", c);
+	return (0);
+}
