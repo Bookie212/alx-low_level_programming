4141cceebaa45066141f8642184b731708f238ab
bookie212 <nancybookie675@gmail.com>
2023-07-20 13:26:32 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints all numbers except 2 & 4
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times of a number
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar("%d", j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
