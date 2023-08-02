2ce2903c2d7b7a0b2674a09061776201fd4ccb16
bookie212 <nancybookie675@gmail.com>
2023-08-02 09:46:49 -0700

    task1

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string to be printed
+ * Return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s == "")
+		return ("");
+	return ((s(1)) + (s(0)));
+}
