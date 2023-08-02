2d638b33fb927e8fc77b89d8403f6a106bf6ff2d
bookie212 <nancybookie675@gmail.com>
2023-08-02 12:40:39 -0700

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
  * is_palindrome - an empty string
- * _strlen_recursion - returns length
  * @s: character
  * Return: Always 0
  */
-int _strlen_recursion(char *s);
 
 int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (1);
 }
